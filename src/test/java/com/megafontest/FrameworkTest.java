5f3555962df8ab05a4d557082e4abb2b6e5f84a0
denisartemov <artemov.dv@yandex.ru>
2024-06-17 22:38:25 +0100

    Initial commit with project setup

---
+++ src/test/java/com/megafontest/FrameworkTest.java
@@ -0,0 +1,12 @@
+package com.megafontest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FrameworkTest {
+    @Test
+    public void testPass() {
+
+        Assert.assertTrue(true);
+    }
+}
